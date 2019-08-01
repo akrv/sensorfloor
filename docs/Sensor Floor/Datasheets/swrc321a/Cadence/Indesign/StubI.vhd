dcdc98710d4a1faf1cdab1ac898bd828a8ea29c9
Aswin <aswinkarthik.rv@gmail.com>
2019-08-01 16:24:41 +0200

    documentation of the PCB

---
+++ docs/Sensor Floor/Datasheets/swrc321a/Cadence/Indesign/StubI.vhd
@@ -0,0 +1,4 @@
+-- Generated PORTMAP Stub File: Created by Capture FPGA Flow
+-- Matches PCB component pinout with simulation model
+-- Created Monday, August 01, 2016 21:44:30 W. Europe Daylight Time
+
---
+++ docs/Sensor Floor/Datasheets/swrc321a/Cadence/Indesign/sdf.cmd
@@ -0,0 +1,2 @@
+LOG_FILE = "c:\kht\work\cc13xx sensortag\manufacture\cadence\Indesign\sdfout.log",
+SCOPE = :dut;
