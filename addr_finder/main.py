5e4087b9e1680217dfe3e5737bb948b0a4a340b8
Aswin <aswinkarthik.rv@gmail.com>
2019-08-22 11:26:18 +0200

    address finder is complete as a module

--- addr_finder/main.py
+++ addr_finder/main.py
@@ -9,8 +9,6 @@ import os
 from subprocess import Popen, PIPE
 
 import serial
-
-list_of_nodes = []
 nodes_inorder = {}
 
 def run_jelmer():
@@ -84,10 +82,12 @@ def set_blm(sensor):
     sleep(0.1) # wait until chip boots to BL mode
     sensor.PIO_BYTE = "150"
 
-def get_all_name():
+def get_all_name(node_list):
+    list_of_nodes=[]
     for sensor in node_list:
         if sensor.type == "DS2408":
             list_of_nodes.append(sensor._path)
+    return list_of_nodes
 
 def set_all_0():
     # zero with comms turned off. so it is 64 and not 0 itself.
@@ -113,37 +113,46 @@ def read_all():
             print(sensor._path, sensor.sensed_BYTE)
 
 def addr_find():
-    nodes_inorder = {}
-    for sensor in node_list:
-        if sensor.type == "DS2408":
-            # if sensor._path in excluded_sensors:
-            #     print("skipping already found sensor %", sensor._path)
-            # else:
-            # print(sensor._path)
-            sensor.PIO_BYTE = "147"  # lsb is PIO0 msb is PIO7
-            ## read data
-            (id,sensor_path) = read_data(sensor._path)
-            sensor.PIO_BYTE = "64"  # lsb is PIO0 msb is PIO7
+    node_list = ow.Sensor('/').sensorList()
+    list_of_nodes = get_all_name(node_list)
+    if len(list_of_nodes) == 15:
+        nodes_inorder_list = range(0,16)
+        for sensor in node_list:
+            if sensor.type == "DS2408":
+                # if sensor._path in excluded_sensors:
+                #     print("skipping already found sensor %", sensor._path)
+                # else:
+                # print(sensor._path)
+                sensor.PIO_BYTE = "147"  # lsb is PIO0 msb is PIO7
+                ## read data
+                (id,sensor_path) = read_data(sensor._path)
+                sensor.PIO_BYTE = "64"  # lsb is PIO0 msb is PIO7
 
-            # check bootloader
-            set_blm(sensor)
-            # get IEEE address
-            ieee_addr = run_jelmer()
-            sensor.PIO_BYTE = "64"  # lsb is PIO0 msb is PIO7
-            nodes_inorder[id] = {
-                                    "wire1" : sensor._path,
-                                    "IEEE"  : ieee_addr,
-                                    "id"    : id
-                                  }
-            pprint(nodes_inorder[id])
-    (json.dumps(nodes_inorder, sort_keys=True))  # probably this helps in sorting the nodes in order
-    # path will work with production as well.
-    with open(os.path.dirname(os.path.realpath(__file__)) + '/node_order.json', 'w') as outfile:
-        json.dump(nodes_inorder, outfile)
-    return nodes_inorder
+                # check bootloader
+                set_blm(sensor)
+                # get IEEE address
+                ieee_addr = run_jelmer()
+                sensor.PIO_BYTE = "64"  # lsb is PIO0 msb is PIO7
+                # this is an ordered list with 0 is 0 and the rest is populated
+                nodes_inorder_list[id] = {
+                                        "wire1" : sensor._path,
+                                        "IEEE"  : ieee_addr,
+                                        "id"    : id
+                                      }
+                pprint(nodes_inorder_list)
+
+        (json.dumps(nodes_inorder, sort_keys=True))  # probably this helps in sorting the nodes in order
+
+        # path will work with production as well.
+        with open(os.path.dirname(os.path.realpath(__file__)) + '/node_order.json', 'w') as outfile:
+            json.dump(nodes_inorder_list, outfile)
+        return ('ok',nodes_inorder_list)
+
+    else:
+        return ('error','not all devices are seen on the wire1 bus')
 if __name__ == '__main__':
     # populate the list
-    get_all_name()
+    list_of_nodes = get_all_name()
     if len(list_of_nodes) == 15:
         # set all nodes to 64
         set_all_0()
@@ -152,7 +161,7 @@ if __name__ == '__main__':
         nodes_inorder = addr_find()
 
         for node in nodes_inorder:
-            pprint(nodes_inorder[node])
+            pprint(node)
         print("ID 0 means the device ID was not found\nIEEE address is EE:EE:... there is problems with bootloader mode.")
     else:
         print("Only %d/15 1wire nodes are seen, fix this" % (len(list_of_nodes)) )
