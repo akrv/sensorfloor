5c5ea6590182e37534f7983a388352d757685281
Aswin <aswinkarthik.rv@gmail.com>
2019-11-23 15:51:26 +0100

    function ready to be used from another program

--- imu_reader/imu_read.py
+++ imu_reader/imu_read.py
@@ -4,10 +4,17 @@ import os, json
 from subprocess import Popen, PIPE
 import ow
 import paho.mqtt.client as paho
-broker="129.217.152.1"
-port=8883
 
-def reader_worker(strip_id,strip_path_inorder):
+def reader_worker(strip_id,strip_path_inorder,node_list,serial_handler, mqtt_connection_info):
+    # strip id is important for constructing the topic
+    # strip_path_inorder is a ordered list of wire1 ID as owfs path
+    # node_list is the object created by owfs lib for list of wire1 devices
+    # serial handler when this function is used from another thread which has access
+    # mqtt connection info is a list with broker and port
+
+    client1 = paho.Client("raspi-dev")  # create client
+    client1.connect(mqtt_connection_info[0], mqtt_connection_info[1])  # establish connection
+
     while continously_run:
         for sensor in node_list:
             if sensor.type == "DS2408":
@@ -37,7 +44,7 @@ def reader_worker(strip_id,strip_path_inorder):
                 all_read = True
                 data_received = []
                 while all_read:
-                    rcvdData = ser.readline()
+                    rcvdData = serial_handler.readline()
                     if rcvdData:
                         data_received.append(rcvdData)
                     if len(data_received)==2:
@@ -51,6 +58,8 @@ def reader_worker(strip_id,strip_path_inorder):
         # print(time()-start_time)
         ret = client1.publish('imu_reader/latency', str(time()-start_time))  # publish
 if __name__ == '__main__':
+    broker = "129.217.152.1"
+    port = 8883
 
     # get the wire1 lib setup to talk the devices
     (ow.init('localhost:4304'))
@@ -61,9 +70,7 @@ if __name__ == '__main__':
     # serial port to talk through rs422
     ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1)
 
-    # mqtt publish
-    client1 = paho.Client("raspi-dev")  # create client
-    client1.connect(broker, port)  # establish connection
+
 
     # read json file from file system
     # next version should read from mongodb (when?)
@@ -74,4 +81,4 @@ if __name__ == '__main__':
     # an ordered list with index == position on strip
     strip_path_inorder = [node_name['wire1'] for node_name in strip_path_inorder]
 
-    reader_worker(strip_id='dev', strip_path_inorder=strip_path_inorder)
+    reader_worker(strip_id='dev', strip_path_inorder=strip_path_inorder,node_list=node_list,serial_handler=ser,mqtt_connection_info=[broker,port])
