042ca14d5f079377b01afd8dcd947dc052b0c7da
Aswin <aswinkarthik.rv@gmail.com>
2019-12-18 18:31:31 +0100

    copy addr to github gists

---
+++ addr_finder/copy_to_gist.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# usage python gist.py ./gist.py [GitHub Username] [GitHub Password]
+import os, requests, sys, json
+
+def getMAC(interface='eth0'):
+    # Return the MAC address of the specified interface
+    try:
+        str = open('/sys/class/net/%s/address' % interface).read()
+    except:
+        str = "00:00:00:00:00:00"
+    return str[0:17]
+
+
+username=sys.argv[2]
+password=sys.argv[3]
+filename = getMAC()
+content=open('node_order.json', 'r').read()
+r = requests.post('https://api.github.com/gists',json.dumps({'files':{filename:{"content":content}}}),auth=requests.auth.HTTPBasicAuth(username, password))
+print(r.json()['html_url'])
