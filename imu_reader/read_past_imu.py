6aba57fdcfeb984c00e018509aea97e50d310016
cmdev2 <info@codingmind.in>
2020-06-08 15:32:12 +0200

    publishing in another namespace to improve redis access

--- imu_reader/read_past_imu.py
+++ imu_reader/read_past_imu.py
@@ -8,6 +8,33 @@ import paho.mqtt.client as paho
 from pprint import pprint
 import struct
 
+RPi_IPs = [
+            {"column_num": 1, "ip_addr": "129.217.152.74", "mac_id": "b8:27:eb:41:99:a0", "hostname": "raspberrypi"},
+            {"column_num": 2, "ip_addr": "129.217.152.111", "mac_id": "b8:27:eb:c0:fd:6a", "hostname": "raspberrypi"},
+            {"column_num": 3, "ip_addr": "129.217.152.79", "mac_id": "b8:27:eb:18:92:c7", "hostname": "raspberrypi"},
+            {"column_num": 4, "ip_addr": "129.217.152.54", "mac_id": "b8:27:eb:53:f2:33", "hostname": "raspberrypi"},
+            {"column_num": 5, "ip_addr": "129.217.152.86", "mac_id": "b8:27:eb:e7:6f:dc", "hostname": "raspberrypi"},
+            # {"column_num": 6, "ip_addr": "129.217.152.110", "mac_id": "b8:27:eb:9b:69:9a", "hostname": "raspberrypi"},
+            {"column_num": 6, "ip_addr": "129.217.152.89", "mac_id": "b8:27:eb:38:4b:07", "hostname": "raspberrypi"},
+            {"column_num": 7, "ip_addr": "129.217.152.84", "mac_id": "b8:27:eb:1b:cf:26", "hostname": "raspberrypi"},
+            {"column_num": 8, "ip_addr": "129.217.152.119", "mac_id": "b8:27:eb:6d:0e:53", "hostname": "raspberrypi"},
+            {"column_num": 9, "ip_addr": "129.217.152.77", "mac_id": "b8:27:eb:b7:a3:b7", "hostname": "raspberrypi"},
+            {"column_num": 10, "ip_addr": "129.217.152.118", "mac_id": "b8:27:eb:be:dc:32", "hostname": "raspberrypi"},
+            {"column_num": 11, "ip_addr": "129.217.152.69", "mac_id": "b8:27:eb:ff:a4:48", "hostname": "raspberrypi"},
+            {"column_num": 12, "ip_addr": "129.217.152.59", "mac_id": "b8:27:eb:a9:7d:4d", "hostname": "raspberrypi"},
+            {"column_num": 13, "ip_addr": "129.217.152.85", "mac_id": "b8:27:eb:c4:f8:c7", "hostname": "raspberrypi"},
+            {"column_num": 14, "ip_addr": "129.217.152.48", "mac_id": "b8:27:eb:e4:43:6d", "hostname": "raspberrypi"},
+            {"column_num": 15, "ip_addr": "129.217.152.63", "mac_id": "b8:27:eb:98:69:6e", "hostname": "raspberrypi"},
+            {"column_num": 16, "ip_addr": "129.217.152.50", "mac_id": "b8:27:eb:75:c7:a2", "hostname": "raspberrypi"},
+            {"column_num": 17, "ip_addr": "129.217.152.37", "mac_id": "b8:27:eb:09:3d:77", "hostname": "raspberrypi"},
+            {"column_num": 18, "ip_addr": "129.217.152.60", "mac_id": "b8:27:eb:05:d8:4d", "hostname": "raspberrypi"},
+            {"column_num": 19, "ip_addr": "129.217.152.64", "mac_id": "b8:27:eb:36:da:22", "hostname": "raspberrypi"},
+            {"column_num": 20, "ip_addr": "129.217.152.62", "mac_id": "b8:27:eb:f5:5d:04", "hostname": "raspberrypi"},
+            {"column_num": 21, "ip_addr": "129.217.152.51", "mac_id": "b8:27:eb:88:8d:56", "hostname": "raspberrypi"},
+            {"column_num": 22, "ip_addr": "129.217.152.87", "mac_id": "b8:27:eb:00:be:93", "hostname": "raspberrypi"},
+            {"column_num": 23, "ip_addr": "129.217.152.33", "mac_id": "b8:27:eb:c0:10:ae", "hostname": "raspberrypi"},
+            ]
+
 
 def reader_worker(strip_id, strip_path_inorder, node_list, serial_handler, mqtt_connection_info):
     # strip id is important for constructing the topic
@@ -16,7 +43,7 @@ def reader_worker(strip_id, strip_path_inorder, node_list, serial_handler, mqtt_
     # serial handler when this function is used from another thread which has access
     # mqtt connection info is a list with broker and port
 
-    client1 = paho.Client(strip_id)  # create client
+    client1 = paho.Client(strip_id[0])  # create client
     client1.connect(mqtt_connection_info[0], mqtt_connection_info[1])  # establish connection
 
     # turn on and put them in a not communicating mode
@@ -48,7 +75,8 @@ def reader_worker(strip_id, strip_path_inorder, node_list, serial_handler, mqtt_
                     node_id = str(1 + strip_path_inorder.index(sensor._path))
 
                     # construct topic for publishing
-                    mqtt_publish_topic = 'imu_reader' + "/" + strip_id + "/" + node_id
+                    mqtt_publish_topic = 'imu_reader' + "/" + strip_id[0] + "/" + node_id
+                    mqtt_publish_topic1 = 'imu_reader' + "/" + strip_id[1] + "/" + node_id
 
                     switching_start_time = time()
                     # set all devices PIO 6 and 7 RX and TX off.
@@ -114,12 +142,14 @@ def reader_worker(strip_id, strip_path_inorder, node_list, serial_handler, mqtt_
                     parsing_latency.append(time() - parsing_start_time)
                     publish_start_time = time()
                     # format data to be published with metadata
-                    data_to_publish = {'strip_id': strip_id,
+                    data_to_publish = {'strip_id': strip_id[0],
+                                       'column_num': strip_id[1],
                                        'node_id': node_id,
                                        'data': reading_to_publish,
                                        'timestamp': time()
                                        }
                     ret = client1.publish(mqtt_publish_topic, json.dumps(data_to_publish))  # publish
+                    ret = client1.publish(mqtt_publish_topic1, json.dumps(data_to_publish))  # publish
                     publish_latency.append(time() - publish_start_time)
                 # print(reading_to_publish)
                 switching2 = time()
@@ -142,9 +172,15 @@ def getMAC(interface='eth0'):
     # Return the MAC address of the specified interface
     try:
         str = open('/sys/class/net/%s/address' % interface).read()
+        for ip in RPi_IPs:
+            if ip["mac_id"] == str:
+                column_num = ip["column_num"]
     except:
         str = "00:00:00:00:00:00"
-    return str[0:17]
+        column_num = 0
+
+
+    return [str[0:17],column_num]
 
 
 if __name__ == '__main__':
@@ -169,5 +205,6 @@ if __name__ == '__main__':
     # an ordered list with index == position on strip
     strip_path_inorder = [node_name['wire1'] for node_name in strip_path_inorder]
     print(getMAC())
+
     reader_worker(strip_id=getMAC(), strip_path_inorder=strip_path_inorder, node_list=node_list, serial_handler=ser,
                   mqtt_connection_info=[broker, port])
