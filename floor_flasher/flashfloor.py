fa465ad605dd3e6f445972a1dc505a197f8295b0
cmdev2 <info@codingmind.in>
2020-10-15 18:50:11 +0200

    code formatted for spacing

--- floor_flasher/flashfloor.py
+++ floor_flasher/flashfloor.py
@@ -6,6 +6,7 @@ from aiohttp import ClientSession
 
 from fabfile import RPi_IPs
 
+
 def send_flash_req(ip_addr, filename, devices=0):
     if filename:
         url = "http://" + ip_addr
@@ -23,7 +24,7 @@ def send_flash_req(ip_addr, filename, devices=0):
 
 def flash_coroutine(RPi_IPs, filename):
     for RPi in RPi_IPs:
-        print("working: ",RPi['ip_addr'])
+        print("working: ", RPi['ip_addr'])
         result = send_flash_req(RPi['ip_addr'], filename)
 
 
@@ -41,29 +42,30 @@ async def fetch(url, session):
     async with session.get(url) as response:
         response_text = await response.text()
         response_status = response.status
-        return (response_status,response_text)
+        return (response_status, response_text)
+
 
 async def post_firmware(url, session, firmwarepath):
     payload = {
-                'device': "0",
-                'file': open(firmwarepath, 'rb')
-               }
+        'device': "0",
+        'file': open(firmwarepath, 'rb')
+    }
     async with session.post(url, data=payload) as response:
         response_text = await response.text()
         response_status = response.status
-        return (response_status,response_text)
+        return (response_status, response_text)
 
 
-async def run(type="check",RPi_IPs=RPi_IPs,firmware=None):
+async def run(type="check", RPi_IPs=RPi_IPs, firmware=None):
     # by default check to disable accidental flash all.
     tasks = []
-    if type=="check":
+    if type == "check":
         # Fetch all responses within one Client session,
         # keep connection alive for all requests.
         async with ClientSession() as session:
             for RPI in RPi_IPs:
                 url = "http://" + RPI['ip_addr']
-                task =asyncio.ensure_future(fetch(url, session))
+                task = asyncio.ensure_future(fetch(url, session))
                 tasks.append(task)
 
             responses = await asyncio.gather(*tasks)
@@ -77,13 +79,14 @@ async def run(type="check",RPi_IPs=RPi_IPs,firmware=None):
         async with ClientSession() as session:
             for RPI in RPi_IPs:
                 url = "http://" + RPI['ip_addr']
-                task = asyncio.ensure_future(post_firmware(url, session,firmware))
+                task = asyncio.ensure_future(post_firmware(url, session, firmware))
                 tasks.append(task)
 
             responses = await asyncio.gather(*tasks)
             # you now have all response bodies in this variable
             for i in responses:
                 print(i)
+
 
 if __name__ == '__main__':
 
@@ -108,26 +111,26 @@ if __name__ == '__main__':
         print(args)
     except:
         print('args provided are wrong')
-        #exit()
+        # exit()
 
     if args.check:
         print('Check all services')
         loop = asyncio.get_event_loop()
         future = asyncio.ensure_future(run("check"))
         loop.run_until_complete(future)
-        #exit()
+        # exit()
 
     if args.firmware_path is None:
         print('Please provide absolute path file name as arg to flash')
-        #exit()brew update && brew upgrade && brew install openssl
+        # exit()brew update && brew upgrade && brew install openssl
 
     if args.flash_all:
         print('Flashing the whole floor')
         loop = asyncio.get_event_loop()
-        future = asyncio.ensure_future(run(type="flashall",RPi_IPs=RPi_IPs, firmware=args.firmware_path))
+        future = asyncio.ensure_future(run(type="flashall", RPi_IPs=RPi_IPs, firmware=args.firmware_path))
         loop.run_until_complete(future)
     elif args.strip_id:
         print('Flashing node strip_id:', args.strip_id, 'node_id:', args.node_id)
-        send_flash_req(RPi_IPs[args.strip_id-1]['ip_addr'], args.firmware_path, devices=args.node_id)
+        send_flash_req(RPi_IPs[args.strip_id - 1]['ip_addr'], args.firmware_path, devices=args.node_id)
     else:
         print("print arg options")
